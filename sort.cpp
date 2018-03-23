266ca6c575ebbc32aca764dd796f0ed6538bbbf5
Jaguar <1413003@saec.ac.in>
2018-03-23 17:38:41 +0530

    my sorting c++ program

--- sort.cpp
+++ sort.cpp
@@ -1,1 +1,15 @@
 /*program to sort an array of size N*/
+int main()
+{
+	long int SUM=0,N,A,D;
+	cin>>N>>A>>D;
+	sortArr(N,A,D);
+	return 0;
+}
+int sortArr(long int N,long int A,int D)
+{
+	int sum;
+	sum=A+(N-1)*D;
+	cout<<sum;
+	return 0;
+}
