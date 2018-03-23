ba0891624d93db9958ed30bdaf47a879b2d0c996
akshaya sridhar <1413003@saec.ac.in>
2018-03-23 17:45:20 +0530

    Update AP.cpp

--- AP.cpp
+++ AP.cpp
@@ -3,7 +3,10 @@ int main()
 {
 	long int SUM=0,N,A,D;
 	cin>>N>>A>>D;
+	if((isDigit(N))&&(isDigit(A))&&(isDigit(D)))
 	sortArr(N,A,D);
+	else
+	cout<<"invalid input";	
 	return 0;
 }
 int sortArr(long int N,long int A,int D)
